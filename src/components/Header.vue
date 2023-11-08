052351a5b6536afbb76c334e3d1511a037da4793
Basel-shibob <shibobbasel@gmail.com>
2023-11-08 05:51:30 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<script>
+import ButtonVue from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    ButtonVue,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  emits: ["toggle-add-task"],
+};
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+    <button-vue
+      v-show="homePage"
+      @toggle-add-task="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      v-for="task in tasks"
+      :key="task.id"
+      :task="task"
+    />
+  </div>
+</template>
+  
+  <script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
